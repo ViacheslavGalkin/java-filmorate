46966235618c15fd99328abbbe4e79c053f66a83
VuacheslavG <galkin.slava@list.ru>
2023-05-10 14:49:45 +0300

    style

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/ValidationException.java
@@ -1,6 +1,5 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
-import org.springframework.http.HttpStatus;
 
 public class ValidationException extends RuntimeException {
     public ValidationException(String message) {
